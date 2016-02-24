309dd7fbb10a4911c64041219ce7a53eb14299f6
Roy Hyunjin Han <rhh@crosscompute.com>
2016-02-24 18:48:41 +0000

    Save geotable examples

--- examples/python/show-maps/show_map_examples.py
+++ examples/python/show-maps/show_map_examples.py
@@ -1,47 +1,40 @@
+import re
 from invisibleroads_macros.disk import make_folder
 from os.path import basename, join
 from shutil import copy
 from sys import argv
 
-target_folder = make_folder(argv[1])
-# Point
-source_path = 'point_geometry.csv'
-target_path = join(target_folder, basename(source_path))
-copy(source_path, target_folder)
-print('point_geometry_example_table_path = ' + target_path)
-print('point_geometry_example_geotable_path = ' + target_path)
-# Geometry
-# print('wkt_geometry_example_geotable_path = ')
 
-# Base
-# print('tile_example_dark_geotable_path')
-# print('tile_example_streets_satellite_geotable_path')
+TARGET_FOLDER = make_folder(argv[1])
+EXAMPLE_PATTERN = re.compile(r'_example.*')
 
-# Radius in pixels
-# print('pixel_radius_example_geotable_path')
-# Radius in meters
-# print('meter_radius_example_geotable_path')
-# Radius range
-# print('range_radius_example_geotable_path')
-# Radius from mean
-# print('mean_radius_example_geotable_path')
-# Radius from sum
-# print('sum_radius_example_geotable_path')
 
-# Fill color
-# !! all variations
-# single letter
-# 0-1 grey shades
-# hex string
-# rgb tuple
-# html names
-# print('specific_color_example_geotable_path')
-# print('mean_specific_color_example_geotable_path')
-# print('sum_specific_color_example_geotable_path')
-# from mean sum
+def prepare_map(map_name, show_table=True):
+    source_name = EXAMPLE_PATTERN.sub('', map_name)
+    source_path = source_name + '.csv'
+    target_path = join(TARGET_FOLDER, basename(source_path))
+    copy(source_path, target_path)
+    if show_table:
+        print('%s_table_path = %s' % (map_name, target_path))
+    print('%s_geotable_path = %s' % (map_name, target_path))
 
-# Fill color scheme
-# from mean sum
-# print('scheme_color_example_geotable_path')
-# print('mean_scheme_color_example_geotable_path')
-# print('sum_scheme_color_example_geotable_path')
+
+prepare_map('point_geometry_example')
+prepare_map('point_geometry_example_dark', show_table=False)
+prepare_map('point_geometry_example_streets_satellite', show_table=False)
+prepare_map('wkt_geometry_example')
+
+prepare_map('pixel_radius_example')
+prepare_map('meter_radius_example')
+prepare_map('range_radius_example')
+prepare_map('mean_radius_example')
+prepare_map('sum_radius_example')
+
+prepare_map('the_specific_color_example')
+prepare_map('mean_specific_color_example')
+prepare_map('sum_specific_color_example')
+
+prepare_map('red_scheme_color_example')
+prepare_map('blue_scheme_color_example')
+prepare_map('mean_blue_scheme_color_example')
+prepare_map('sum_blue_scheme_color_example')
